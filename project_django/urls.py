c383eace535a7a5297ffb4d2fbb70d102f9eb7ff
jihanifah <jihanhanifahyasmin12@gmail.com>
2022-09-12 21:13:23 +0700

    finished tugas 2

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('katalog.urls')),
+    path('katalog/', include('katalog.urls')),
 ]
