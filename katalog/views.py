7a6d1aa126bf3727e11a15cec25254ae2ea060c0
jihanifah <jihanhanifahyasmin12@gmail.com>
2022-09-12 18:11:12 +0700

    finished tugas 2 - jihan

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context  ={
+        'item_catalog'  : data_catalog_item,
+        'nama'          : 'Jihan Hanifah Yasmin',
+        'npm'           : 2106701955
+    }
+    return render(request, "katalog.html", context)
