7377c37f10eb311bfbc83cf78c8a6b3eedcc1163
jihanifah <jihanhanifahyasmin12@gmail.com>
2022-10-12 22:04:36 +0700

    finished tugas 6

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todolist.views import show_task, register, login_user, logout_user,add_task
+from todolist.views import show_task, add, register, login_user, logout_user,add_task, show_json
 
 app_name = 'todolist'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('create-task/', add_task, name='create-task'),
+    path('/json', show_json, name='show_json'),
+    path('/add', add, name='add'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from todolist.forms import TodoForm
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core import serializers
 
 @login_required(login_url='/todolist/login/')
 def show_task(request):
@@ -17,6 +19,20 @@ def show_task(request):
         'task_list' : task_data,
     }
     return render(request, "todolist.html", context)
+
+def show_json(request):
+    data = Task.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", data))
+
+def add(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        data = Task(user=request.user,title=title, description=description)
+        data.save()
+        return HttpResponse(b"CREATED", status=201)
+    return HttpResponseNotFound()
 
 def register(request):
     form = UserCreationForm()
